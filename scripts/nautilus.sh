094350f0c535277f810d5ac4a4458aad0f2f76b9
Ahaupt3 <alexhaupt3@gmail.com>
2020-11-04 16:24:04 -0800

    Fixed apt loop & set pip install to quiet

--- scripts/nautilus.sh
+++ scripts/nautilus.sh
@@ -3,10 +3,10 @@
 nautilus(){
     echo -e "${GREEN}Installing Nautilus plugins${YELLOW}"
     sudo apt-get install -y -qq nautilus-admin > /dev/null
-    pip install nautilus-open-any-terminal
+    pip -q install nautilus-open-any-terminal
     glib-compile-schemas ~/.local/share/glib-2.0/schemas/
     gsettings set com.github.stunkymonkey.nautilus-open-any-terminal terminal terminator
-    sudo apt purge --autoremove -y -qq nautilus-extension-gnome-terminal > /dev/null
+    sudo apt-get purge --autoremove -y -qq nautilus-extension-gnome-terminal > /dev/null
     nautilus -q
     echo -e ""
 
