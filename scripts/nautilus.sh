a493eddce99368342def3b2f94eedd47b653e9c5
Ahaupt3 <alexhaupt3@gmail.com>
2020-11-05 00:17:23 -0800

    Fixed install loop cause by nautilus -q

--- scripts/nautilus.sh
+++ scripts/nautilus.sh
@@ -2,12 +2,19 @@
 
 nautilus(){
     echo -e "${GREEN}Installing Nautilus plugins${YELLOW}"
+    echo -e ""
+
+    which python3-nautilus > /dev/null || sudo apt-get install -y -qq python3-nautilus > /dev/null
+
+    echo -e "${GREEN}Installing plugin nautilus-admin${YELLOW}"
     sudo apt-get install -y -qq nautilus-admin > /dev/null
-    pip -q install nautilus-open-any-terminal
+    echo -e ""
+
+    echo -e "${GREEN}Installing plugin nautilus-open-any-terminal${YELLOW}"
+    pip install -q nautilus-open-any-terminal
     glib-compile-schemas ~/.local/share/glib-2.0/schemas/
     gsettings set com.github.stunkymonkey.nautilus-open-any-terminal terminal terminator
-    sudo apt-get purge --autoremove -y -qq nautilus-extension-gnome-terminal > /dev/null
-    nautilus -q
+    sudo apt purge --autoremove -y nautilus-extension-gnome-terminal > /dev/null
     echo -e ""
 
     echo -e "${GREEN}Finished Installing Nautilus plugins"
