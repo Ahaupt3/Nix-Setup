86282d053f8fe106e9eaec908acd5d1726b39d63
Ahaupt3 <alexhaupt3@gmail.com>
2020-11-04 21:58:31 -0800

    Moved package update from main script to here

--- scripts/preconfig.sh
+++ scripts/preconfig.sh
@@ -7,16 +7,23 @@ preconfig(){
     echo -e "${GREEN}Pre-Configuration"
     echo -e ""
 
+    echo -e "${GREEN}Updating Packages${YELLOW}"
+    if [[ "$OS" == Ubuntu ]]; then
+        sudo add-apt-repository universe
+    fi
+    sudo apt -q update
+    echo -e ""
+
     echo -e "${GREEN}Checking for commands - installing if not found${YELLOW}"
     for CMD in "${CMDS[@]}"; do
         echo -e "${YELLOW}Looking for $CMD"
         which "$CMD" > /dev/null || sudo apt-get install -y -qq "$CMD" > /dev/null
     done
-    echo -e "${GREEN}All necessary commands are installed"
+    echo -e "${GREEN}All prerequisite commands are installed"
     echo -e ""
     
     echo -e "${GREEN}Linking Python3/Pip3 to Python/Pip${YELLOW}"
-    if [ "$(realpath \"$(which python)\")" = "$(realpath \"$(which python3)\")" ]; then
+    if [ "$(realpath "$(which python)")" = "$(realpath "$(which python3)")" ]; then
         :
     else
         sudo rm "$(which python)"
