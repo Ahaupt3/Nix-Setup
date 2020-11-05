42572b078b1ebc0f9dd97b2bb0dfa465b4e7285f
Ahaupt3 <alexhaupt3@gmail.com>
2020-11-04 16:23:28 -0800

    Added check for prerequisite commands & updated python/pip linking

--- scripts/preconfig.sh
+++ scripts/preconfig.sh
@@ -16,15 +16,13 @@ preconfig(){
     echo -e ""
     
     echo -e "${GREEN}Linking Python3/Pip3 to Python/Pip${YELLOW}"
-    if [ "$(realpath "$(which python)")" = "$(realpath "$(which python3)")" ]; then
+    if [ "$(realpath \"$(which python)\")" = "$(realpath \"$(which python3)\")" ]; then
         :
     else
-        sudo rm /usr/bin/python
-        sudo ln -s /usr/bin/python3 /usr/bin/python
-        sudo apt-get install -y -qq python3-pip  > /dev/null
-        sudo rm /usr/bin/pip
-        sudo ln -s /usr/bin/pip3 /usr/bin/pip
+        sudo rm "$(which python)"
+        sudo ln -s "$(which python3)" /usr/bin/python
     fi
+    which pip > /dev/null || sudo apt-get install -y -qq python3-pip > /dev/null
     echo -e ""
 
     echo -e "${GREEN}Finished Pre-Configuration"
