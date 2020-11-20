4aa4cbcc553c780da579eefdc889015843e86cf8
Ahaupt3 <alexhaupt3@gmail.com>
2020-11-20 08:25:31 -0800

    Removed python3 symlinking

--- scripts/preconfig.sh
+++ scripts/preconfig.sh
@@ -19,13 +19,6 @@ preconfig(){
     done
     echo -e "\n${GREEN}All prerequisite commands are installed"
         
-    echo -e "\n${GREEN}Linking Python3/Pip3 to Python/Pip${YELLOW}"
-    if [ "$(realpath "$(which python)")" = "$(realpath "$(which python3)")" ]; then
-        :
-    else
-        sudo rm "$(which python)"
-        sudo ln -s "$(which python3)" /usr/bin/python
-    fi
     which pip > /dev/null || sudo apt-get install -y -qq python3-pip > /dev/null
     
     echo -e "\n${GREEN}Finished Pre-Configuration"
